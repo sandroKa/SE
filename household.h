7eafca7efb603fa94f3ba383ad8d3105c021a682
Birk Kauer <Trax@MacBook-Pro.local>
2015-10-24 19:35:08 +0200

    Login Possible and everything is setup for coding

---
+++ household.h
@@ -0,0 +1,11 @@
+#ifndef HOUSEHOLD_H
+#define HOUSEHOLD_H
+
+
+class Household
+{
+public:
+    Household();
+};
+
+#endif // HOUSEHOLD_H
---
+++ idata.h
@@ -0,0 +1,5 @@
+#ifndef IDATA
+#define IDATA
+
+#endif // IDATA
+
